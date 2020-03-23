d6028a95c3075877f3124a0f052d49c189e7a507
Michael Mendoza <michael.mend97@yahoo.com>
2020-03-23 18:15:57 -0400

    WIP

--- server.js
+++ server.js
@@ -2,3 +2,49 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const fs = require('fs');
+
+let noteData = require('./develop/db/db.json');
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.static("develop/public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/index.html'));
+})
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(noteData);
+})
+
+app.post('/api/notes', (req, res) => {
+    noteData.push(req.body);
+    noteData.forEach((note, i) => {
+        note.id = i + 1;
+    })
+    let newNote = JSON.stringify(noteData);
+    fs.writeFileSync('./develop/db/db.json', newNote);
+
+    res.json(noteData);
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+
+    let filtered = noteData.filter(note => note.id !== parseInt(req.params.id));
+    fs.writeFileSync('./develop/db/db.json', JSON.stringify(filtered));
+
+    noteData = filtered;
+
+    res.json(noteData);
+
+})
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
